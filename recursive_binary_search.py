dd9e2aee4f212882668574338d682afaca67cc64
Raul Giron (he/him/his) <raulgiron@hotmail.com>
2022-02-15 00:30:14 -0400

    Add alternative recursive binary search algorithm.

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -8,20 +8,39 @@ def recursive_binary_search(data_list, target):
             return True
         else:
             if data_list[midpoint] < target:
-                return recursive_binary_search(data_list[midpoint+1:], target)
+                return recursive_binary_search(data_list[midpoint + 1:], target)
             else:
                 return recursive_binary_search(data_list[:midpoint], target)
+
+
+def alternative_recursive_binary_search(data_list: list, target: int, start: int = 0, end=None) -> int | None:
+    if end is None:
+        end = len(data_list) - 1
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if target == data_list[mid]:
+        return mid
+    else:
+        if target < data_list[mid]:
+            return alternative_recursive_binary_search(data_list, target, start, mid - 1)
+        else:
+            return alternative_recursive_binary_search(data_list, target, mid + 1, end)
 
 
 def verify(result):
     print("Target found: ", result)
 
 
-numbers1 = []
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-result = recursive_binary_search(numbers1, 6)
+empty_list = []
+numbers_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = recursive_binary_search(numbers_list, 12)
 verify(result)
-result = recursive_binary_search(numbers, 12)
+result = recursive_binary_search(numbers_list, 6)
 verify(result)
-result = recursive_binary_search(numbers, 6)
+result = alternative_recursive_binary_search(numbers_list, 12)
+verify(result)
+result = alternative_recursive_binary_search(numbers_list, 6)
 verify(result)
